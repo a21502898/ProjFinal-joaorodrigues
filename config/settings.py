3b56a9450f40e12c33fd14c640695b42b592fd21
Joao Rodrigues <kiko.jffr@gmail.com>
2021-09-15 14:28:56 +0000

    commit inicial

--- config/settings.py
+++ config/settings.py
@@ -131,8 +131,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('website/static'))]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
